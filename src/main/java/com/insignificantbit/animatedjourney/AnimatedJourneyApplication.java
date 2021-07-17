0cead3dad0b1c8172e25351679f232349cd067c5
Sougata Khan <sougata.khan@volt.com.au>
2021-07-17 17:08:27 +1000

    Initial Commit

---
+++ src/main/java/com/insignificantbit/animatedjourney/AnimatedJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.insignificantbit.animatedjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimatedJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimatedJourneyApplication.class, args);
+	}
+
+}
